0c80cb314e7e3ecf73de2feec5349c04c0dd581b
hexlism <hexhexlism@gmail.com>
2015-12-28 00:32:59 +0800

    complie done

---
+++ sleepyenv/bin/pilprint.py
@@ -0,0 +1,4 @@
+#!/home/firlism/tools/css_platform/sleepyenv/bin/python
+# EASY-INSTALL-SCRIPT: 'Pillow==2.8.2','pilprint.py'
+__requires__ = 'Pillow==2.8.2'
+__import__('pkg_resources').run_script('Pillow==2.8.2', 'pilprint.py')
